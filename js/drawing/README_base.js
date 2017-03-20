b0343e1133031f413405bbea174b968bd8296ce1
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-03-20 13:27:01 +0100

    Remove redundant semtonotes files

--- js/drawing/README_base.js
+++
@@ -1,2 +0,0 @@
-base.js ist gegen die minified-Version derselben Datei getauscht. Original unter base.js.orig.
-Da andere JS-Module vom Namen base.js ausgehen (beim Nachladen von deps.js), konnte Name nicht in base.min.js geändert werden
