8605780d00e34ff93a9267fdc461625b149dee06
Konstantin Baierer <unixprog@gmail.com>
2017-04-04 22:06:17 +0200

    fix anno-editor to work with vuex store

--- src/components/anno-editor.js
+++ src/components/anno-editor.js
@@ -1,30 +1,24 @@
 const AnnoClient = require('../anno-client')
 module.exports = {
     props: {
-        annotation: {type: Object, required: true},
         targetImage: {type: String, required: true},
         targetThumbnail: {type: String},
         l10n: {type: Object, required: true},
     },
     template: require('./anno-editor.html'),
     style: require('./anno-editor.css'),
-    components: {
-        'bootstrap-button': require('./bootstrap-button'),
-        'bootstrap-tabs':   require('./bootstrap-tabs'),
-        'bootstrap-tab':    require('./bootstrap-tab'),
-        'zone-editor':      require('./zone-editor'),
-        'html-editor':      require('./html-editor'),
-        'tags-editor':      require('./tags-editor'),
-        'semtags-editor':   require('./semtags-editor'),
-    },
     mounted() {
         this.client = AnnoClient.instance
+    },
+    computed: {
+        id() { return this.$store.state.id },
+        stateDump() { return this.$store.state },
     },
     methods: {
         save() {
             console.log("Save called")
-            this.client.createOrRevise(this.annotation)
-                .then(resp => this.annotation = resp.data)
+            this.client.createOrRevise(this.$store.state)
+                .then(resp => this.$store.commit('replace', resp.data))
                 .catch(err => console.log("ERROR", err))
         }
     },
