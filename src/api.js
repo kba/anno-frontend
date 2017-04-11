354a8720ce6923bfbc0ab50696a8142c6a780952
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-04-11 15:45:38 +0200

    make clearer that apiFactory is to be passed the state

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const AnnoStoreHttp = require('@kba/anno-store-http')
 const httpHeadersMiddleware = require('@kba/anno-mw-httpheaders')
 
-module.exports = (config) => {
+module.exports = (state) => {
     const annoStore = new AnnoStoreHttp({
         STORE: '@kba/anno-store-http',
-        BASE_URL: config.endpoint,
+        BASE_URL: state.annoEndpoint,
         // TODO get token from state
-        HTTP_HEADERS: `Authorization: Bearer ${config.token}`,
+        HTTP_HEADERS: `Authorization: Bearer ${state.token}`,
     })
     annoStore.use(httpHeadersMiddleware())
     return annoStore
