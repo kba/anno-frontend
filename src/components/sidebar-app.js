9d37e6ce87ca99239ecc32e579208855f7e09ad6
Konstantin Baierer <unixprog@gmail.com>
2017-04-11 23:16:12 +0200

    styling

--- src/components/sidebar-app.js
+++ src/components/sidebar-app.js
@@ -1,6 +1,7 @@
-const $ = require('jquery')
-
 module.exports = {
+    props: {
+        collapseInitially: {type: Boolean, default: false},
+    },
     mixins: [
         require('../mixin/prefix'),
         require('../mixin/l10n'),
@@ -8,28 +9,12 @@ module.exports = {
     style: require('./sidebar-app.scss'),
     template: require('./sidebar-app.html'),
     data() { return {
-        collapsed: false,
+        collapsed: this.collapseInitially,
     }},
     computed: {
         numberOfAnnotations() { return this.$store.getters.numberOfAnnotations },
     },
     methods: {
-
-        toggle() {
-            this.collapsed = !this.collapsed;
-            this[this.collapsed ? 'hide' : 'show']()
-        },
-
-        show() {
-            $('[data-toggle="collapse"]', this.$el).removeClass('collapsed')
-            $('.anno-sidebar-body').removeClass('hidden')
-            $('.anno-sidebar-body').removeClass('hidden-print')
-        },
-
-        hide() {
-            $('[data-toggle="collapse"]', this.$el).addClass('collapsed')
-            $('.anno-sidebar-body').addClass('hidden')
-            $('.anno-sidebar-body').addClass('hidden-print')
-        },
+        toggle() { this.collapsed = !this.collapsed },
     }
 }
