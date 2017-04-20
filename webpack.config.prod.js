8adc1d463821017b8abc205cad271759834d64af
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-04-20 18:20:13 +0200

    reduce prod bundle size down to about 200k

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,25 +11,27 @@ module.exports = {
         publicPath: 'http://anno.ub.uni-heidelberg.de/dist2/',
     },
     externals: {
-        jquery: "$",
+        'jquery': "$",
         'quill/dist/quill.js': 'Quill',
-        bootstrap: 'bootstrap',
-        vue: 'vue',
-        vuex: 'vuex',
-        'semtonotes-client': 'xrx',
+        'axios': 'axios',
         'semtonotes-utils': 'XrxUtils',
+        '@kba/anno-store': 'Anno.Store',
+        '@kba/anno-store-http': 'Anno.HttpStore',
+        '@kba/anno-errors': 'Anno.Errors',
     },
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.common.js',
             'quill$': 'quill/dist/quill.min.js',
             'async$': 'async/dist/async.min.js',
+            // 'axios': 'axios/dist/axios.min.js',
         }
     },
     module: {
         loaders: [
             {test: /png$/i, loader: "file-loader"},
             {test: /components\/.*?\.html$/, loader: "html-loader?attrs=img:src bootstrap-button:src" },
+            {test: /.*\.js$/, exclude: /node_modules/, loader: 'babel-loader?cacheDirectory'},
             {
                 test: /components\/.*?\.s?css$/,
                 loader: "style-loader!css-loader?sourcemap=true!sass-loader?sourcemap-=true"
