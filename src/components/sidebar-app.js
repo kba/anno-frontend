b60d0cdc27c039bed52c15babd0251edc9678dd1
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-04-20 10:44:34 +0200

    Make sidebar optionally not standalone

--- src/components/sidebar-app.js
+++ src/components/sidebar-app.js
@@ -1,6 +1,22 @@
+/*
+ * ### sidebar-app
+ *
+ * Container for the list of annotations for a target and a modal editor to
+ * edit/create new annotations.
+ *
+ * #### Props
+ *
+ * - `collapseInitially`: Whether the annotation list should be collapsed after loading
+ * - `standalone`: Whether the sidebar should inject it's own
+ *   toggleing/container elements or reuse existing DOM elements. If the
+ *   latter, `el` must be set. See [`displayAnnotations`](#displayannotations)
+ *
+ *
+ */
 module.exports = {
     props: {
         collapseInitially: {type: Boolean, default: false},
+        standalone: {type: Boolean, default: false },
     },
     mixins: [
         require('../mixin/prefix'),
