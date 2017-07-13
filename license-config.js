a37c96a3c3f2261827373f0b24d8ef966263e09b
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-07-13 12:22:50 +0200

    l10n/licenses: differentiate between user and author help

--- license-config.js
+++ license-config.js
@@ -4,13 +4,15 @@ module.exports = {
         img_88x31: require('./img/cc-zero-88x31.svg'),
         img_64x64: require('./img/cc-zero-64x64.svg'),
         title: 'Creative Commons CC0',
-        desc: 'license_cc0_desc',
+        author_desc: 'license_cc0_author',
+        user_desc: 'license_cc0_user',
     },
     cc_by: {
         url: 'https://creativecommons.org/licenses/by/4.0/',
         img_88x31: require('./img/cc-by-88x31.svg'),
         img_64x64: require('./img/cc-by-64x64.svg'),
         title: 'Creative Commons CC-BY 4.0',
-        desc: 'license_ccby_desc',
+        author_desc: 'license_ccby_author',
+        user_desc: 'license_ccby_user',
     }
 }
