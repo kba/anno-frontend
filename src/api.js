157d9f327ad7d6b46e0ea9c37b3198fd7f77be6b
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-10-25 15:04:56 +0200

    :art:

--- src/api.js
+++ src/api.js
@@ -3,14 +3,10 @@ const AnnoStoreHttp = require('@kba/anno-store-http')
 module.exports = (state) => {
     // console.log("Anno Endpoint", state.annoEndpoint)
     const HTTP_HEADERS = {}
-    if (state.token)
-        HTTP_HEADERS['Authorization'] = `Bearer ${state.token}`
-    if (state.collection)
-        HTTP_HEADERS['X-Anno-Collection'] = state.collection
-    if (state.targetSource)
-        HTTP_HEADERS['X-Anno-Context'] = state.targetSource
-    if (state.metadataToken)
-        HTTP_HEADERS['X-Anno-Metadata'] = state.metadataToken
+    if (state.token)           HTTP_HEADERS['Authorization']     = `Bearer ${state.token}`
+    if (state.collection)      HTTP_HEADERS['X-Anno-Collection'] = state.collection
+    if (state.targetSource)    HTTP_HEADERS['X-Anno-Context']    = state.targetSource
+    if (state.metadataToken)   HTTP_HEADERS['X-Anno-Metadata']   = state.metadataToken
     const annoStore = new AnnoStoreHttp({
         BASE_URL: state.annoEndpoint,
         HTTP_HEADERS: JSON.stringify(HTTP_HEADERS)
