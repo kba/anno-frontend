68885d9a66111110071c14e24f3cb49068b7d2ac
mk-pmb <mk-pmb@users.noreply.github.com>
2021-04-21 21:24:47 +0200

    Add deploy option to zipball script

--- test/html/zipball_nm.sh
+++ test/html/zipball_nm.sh
@@ -7,13 +7,13 @@ function zipball_nm () {
   local SELFPATH="$(readlink -m -- "$BASH_SOURCE"/..)"
   cd -- "$SELFPATH"/../.. || return $?
 
-  local DEST='test.nm.snapshot.zip'
+  local ZIP_FN='test.nm.snapshot.zip'
   local NM_HTML=(
     test/html/*.nm.html
     )
   local NM_DEPS=()
   readarray -t NM_DEPS < <(
-    grep -oPe '"(\.*/)*node_modules/[^"]+' -- "${NM_HTML[@]}" \
+    grep -hoPe '"(\.*/)*node_modules/[^"]+' -- "${NM_HTML[@]}" \
     | sed -re 's~^["./]+~~' | sort --version-sort --unique)
   local TEST_DEPS=()
   readarray -t TEST_DEPS < <(find test/ -type f -name '*.js')
@@ -25,8 +25,37 @@ function zipball_nm () {
     "${TEST_DEPS[@]}"
     )
 
-  zip -r9 "$DEST" -- "${PACK_FILES[@]}" || return $?
-  mv --verbose --target-directory="$SELFPATH" -- "$DEST" || return $?
+  zip -r9 "$ZIP_FN" -- "${PACK_FILES[@]}" || return $?
+  mv --verbose --target-directory="$SELFPATH" -- "$ZIP_FN" || return $?
+
+  case " $* " in
+    *' deploy '* ) deploy_to '.git/@/htdocs' .htaccess || return $?;;
+  esac
+}
+
+
+function deploy_to () {
+  local DEST_DIR="$1"; shift
+  local CHECK_FILE="$1"; shift
+  [ -z "$CHECK_FILE" ] || CHECK_FILE="$DEST_DIR/$CHECK_FILE"
+
+  local ZIP_ABS="$SELFPATH/$ZIP_FN"
+  local HASH="$(sha1sum --binary -- "$ZIP_ABS")"
+  HASH="${HASH:0:8}"
+  [ -n "$HASH" ] || return 4$(echo "E: failed to checksum $ZIP_ABS" >&2)
+  local SUBDIR="$DEST_DIR/$(date +%y%m%d-%H%M)-$HASH"
+  echo "Gonna deploy to: $SUBDIR"
+  [ -z "$CHECK_FILE" ] || [ -f "$CHECK_FILE" ] || return 4$(
+    echo 'E: check file missing! Is the webspace mounted?' >&2)
+
+  local UNZIP_OPTS=(
+    -o    # overwrite existing files
+    )
+  [ -d "$SUBDIR" ] || mkdir -- "$SUBDIR" || return $?$(
+    echo "E: failed to mkdir $SUBDIR" >&2)
+  ( cd "$SUBDIR" && unzip "${UNZIP_OPTS[@]}" -- "$ZIP_ABS" ) || return $?$(
+    echo "E: failed to deploy to $SUBDIR" >&2)
+  echo "Deployed to: $SUBDIR"
 }
 
 
