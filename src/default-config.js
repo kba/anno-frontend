88d2b0f4ca0c44099c53137b618a604933414de0
mk-pmb <mk-pmb@users.noreply.github.com>
2021-07-01 04:41:20 +0200

    Factor out default options

---
+++ src/default-config.js
@@ -0,0 +1,15 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+function decide() {
+  const cfg = {
+    container: '…container',
+    prefix: 'anno-app-',
+    targetSource: window.location.href,
+  };
+  return cfg;
+};
+
+
+
+module.exports = decide;
