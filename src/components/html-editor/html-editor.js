44dd17caa49c4bd1998aee3e062607c636d6258f
mk-pmb <mk-pmb@users.noreply.github.com>
2021-07-05 06:27:16 +0200

    HTML editor: Disable debug output on text-change

--- src/components/html-editor/html-editor.js
+++ src/components/html-editor/html-editor.js
@@ -52,7 +52,7 @@ module.exports = {
             theme: 'snow',
         })
         this.quill.on('text-change', (delta, oldDelta, source) => {
-            console.log(this.$refs.editor)
+            // console.debug('HTML editor text change in:', this.$refs.editor);
             let html = this.$refs.editor.children[0].innerHTML
             if (html === '<p><br></p>') html = ''
             this.value = html
