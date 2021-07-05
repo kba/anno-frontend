f48acf90e65806475085130081233519b21cbd10
mk-pmb <mk-pmb@users.noreply.github.com>
2021-07-05 06:26:48 +0200

    anno-viewer-thumbnail-svg: Normalize indentation

--- src/components/anno-viewer-thumbnail-svg/index.js
+++ src/components/anno-viewer-thumbnail-svg/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-    template: require('./thumb.html'),
-    props: {
-        svgTarget: { type: [Object] },
-        sizePx:   { type: [Number], default: 36 },
-        widthPx:  { type: [Number, undefined] },
-        heightPx: { type: [Number, undefined] },
-    },
+  template: require('./thumb.html'),
+  props: {
+    svgTarget:    { type: [Object] },
+    sizePx:       { type: [Number], default: 36 },
+    widthPx:      { type: [Number, undefined] },
+    heightPx:     { type: [Number, undefined] },
+  },
 }
