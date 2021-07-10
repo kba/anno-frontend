0ab730d76cd839c18f7c448df5b32864830f597f
mk-pmb <mk-pmb@users.noreply.github.com>
2021-07-11 01:40:52 +0200

    Relation links: Show URL if no title is given. [ubhd:108]

--- test/html/fixtures.js
+++ test/html/fixtures.js
@@ -1,96 +1,109 @@
+'use strict';
+
 window.annotations = [
   {
     id: 'https://anno.ub.uni-heidelberg.de/123',
     creator: 'john',
     title: 'Anno mit Anwo 1',
     body: [
-      { type: 'TextualBody', format: 'text/html', value: 'Some <strong>bold</strong> statements here.', },
-      { type: 'TextualBody', motivation: 'tagging', value: '#nuffsaid', },
-      { motivation: 'linking', label: 'Metavariables', id: 'http://gnd.info/12345', }
+      { type: 'TextualBody', format: 'text/html', value: 'Some <strong>bold</strong> statements here.' },
+      { type: 'TextualBody', motivation: 'tagging', value: '#nuffsaid' },
+      { motivation: 'linking', label: 'Metavariables', id: 'http://gnd.info/12345' },
     ],
     target: 'http://foo.bar',
-    created:  "2017-03-01T10:00:00Z",
-    modified: "2017-03-22T22:05:00Z",
+    created:  '2017-03-01T10:00:00Z',
+    modified: '2017-03-22T22:05:00Z',
     hasVersion: [
       { title: 'Tset anontatio',
         id: 'https://anno.ub.uni-heidelberg.de/123-rev1',
-        created:  "2017-03-01T10:00:00Z",
-        modified: "2017-03-01T10:05:00Z",
-        doi: "10.1234/123~1"
+        created:  '2017-03-01T10:00:00Z',
+        modified: '2017-03-01T10:05:00Z',
+        doi: '10.1234/123~1',
       },
       { title: 'Tset annotatio (fixed typo)',
         id: 'https://anno.ub.uni-heidelberg.de/123-rev2',
-        created:  "2017-03-01T11:00:00Z",
-        modified: "2017-03-11T11:05:00Z",
-        doi: "10.1234/123~2"
+        created:  '2017-03-01T11:00:00Z',
+        modified: '2017-03-11T11:05:00Z',
+        doi: '10.1234/123~2',
       },
       { title: 'Test annotation (fixed 2nd typo)',
         id: 'https://anno.ub.uni-heidelberg.de/123-rev3',
-        created:  "2017-03-22T22:00:00Z",
-        modified: "2017-03-22T22:05:00Z",
-        doi: "10.1234/123~3"
-      }
+        created:  '2017-03-22T22:00:00Z',
+        modified: '2017-03-22T22:05:00Z',
+        doi: '10.1234/123~3',
+      },
     ],
     hasReply: [
       {
         id: 'https://anno.ub.uni-heidelberg.de/456',
         creator: 'mike',
         title: 'Disagreement',
-        body: { type: 'TextualBody', format: 'text/html', value: 'Too strong, actually', },
-        target: { id: 'https://anno.ub.uni-heidelberg.de/123' }
-      }
+        body: { type: 'TextualBody', format: 'text/html', value: 'Too strong, actually' },
+        target: { id: 'https://anno.ub.uni-heidelberg.de/123' },
+      },
     ],
-    doi: "10.11234/123"
+    doi: '10.11234/123',
   },
 
   {
-    '@context': "http://anno.ub.uni-heidelberg.de/context.jsonld",
+    '@context': 'http://anno.ub.uni-heidelberg.de/context.jsonld',
     body: [
       {
-        format: "text/html",
-        id: "http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&body=1&rev=1",
-        type: "TextualBody",
-        value: ""
-      }
+        format: 'text/html',
+        id: 'http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&body=1&rev=1',
+        type: 'TextualBody',
+        value: '',
+      },
+      {
+        predicate: '',
+        purpose: 'linking',
+        source: 'http://mit.titel.test/',
+        value: 'Mit Titel',
+      },
+      {
+        predicate: '',
+        purpose: 'linking',
+        source: 'http://ohne.titel.test/',
+      },
     ],
-    title: 'foo',
-    created: "2017-03-31 11:22:27",
-    creator: "kba",
+    title: 'Links mit und ohne Titel',
+    created: '2017-03-31 11:22:27',
+    creator: 'kba',
     hasVersion: [
       {
-        id: "http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&rev=1",
-        modified: "2017-03-31 11:22:27",
-      }
+        id: 'http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&rev=1',
+        modified: '2017-03-31 11:22:27',
+      },
     ],
-    id: "http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&rev=1",
+    id: 'http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&rev=1',
     target: [
       {
-        id: "http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&target=1&rev=1",
+        id: 'http://anno.ub.uni-heidelberg.de/cgi-bin/anno.cgi?id=802168b6-873f-4194-88b2-8e6a29a1493b&target=1&rev=1',
         selector: {
-          type: "SvgSelector",
-          value: '<svg xmlns="http://www.w3.org/2000/svg"> <rect x="0" y="0" width="200" height="200"/> <rect x="200" y="200" width="200" height="200"/> <rect x="400" y="400" width="200" height="200"/> <rect x="600" y="600" width="200" height="200"/> <rect x="800" y="800" width="200" height="200"/> <rect x="800" y="1000" width="224" height="24"/> </svg>'
+          type: 'SvgSelector',
+          value: '<svg xmlns="http://www.w3.org/2000/svg"> <rect x="0" y="0" width="200" height="200"/> <rect x="200" y="200" width="200" height="200"/> <rect x="400" y="400" width="200" height="200"/> <rect x="600" y="600" width="200" height="200"/> <rect x="800" y="800" width="200" height="200"/> <rect x="800" y="1000" width="224" height="24"/> </svg>',
         },
-        source: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Ghostscript_Tiger.svg/1024px-Ghostscript_Tiger.svg.png"
-      }
-    ]
+        source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Ghostscript_Tiger.svg/1024px-Ghostscript_Tiger.svg.png',
+      },
+    ],
   },
 
   {
     id: 'http://bla/tags-anno',
     title: 'Tagging and svg test',
     body: [
-      { motivation: 'tagging', value: 'tag0', },
-      { motivation: 'tagging', value: 'tag1', },
-      { motivation: 'tagging', value: 'tag2', },
-      { motivation: 'tagging', value: 'tag3', },
-      { motivation: 'tagging', value: 'tag4', },
-      { motivation: 'tagging', value: 'tag5', },
+      { motivation: 'tagging', value: 'tag0' },
+      { motivation: 'tagging', value: 'tag1' },
+      { motivation: 'tagging', value: 'tag2' },
+      { motivation: 'tagging', value: 'tag3' },
+      { motivation: 'tagging', value: 'tag4' },
+      { motivation: 'tagging', value: 'tag5' },
     ],
     target: [
       {
         source: 'http://foo',
         selector: {
-          type: "SvgSelector",
+          type: 'SvgSelector',
           value: `<svg xmlns="http://www.w3.org/2000/svg">
               <rect x="0" y="0" width="200" height="200"/>
               <rect x="200" y="200" width="200" height="200"/>
@@ -99,7 +112,7 @@ window.annotations = [
               <rect x="800" y="800" width="200" height="200"/>
               <rect x="800" y="1000" width="224" height="24"/>
             </svg>`,
-        }
+        },
       },
     ],
   },
@@ -123,26 +136,26 @@ window.annotations = [
         motivation: 'linking',
         label: 'Metavariables',
         id: 'http://gnd.info/12345',
-      }
+      },
     ],
-    target: {source: 'http://foo.bar' },
-    modified: "2017-03-22 22:00:00",
+    target: { source: 'http://foo.bar' },
+    modified: '2017-03-22 22:00:00',
     hasVersion: [
       {
         id: 'https://anno.ub.uni-heidelberg.de/123-rev1',
         title: 'Tset anontatio',
-        modified: "2017-03-01 10:00:00",
+        modified: '2017-03-01 10:00:00',
       },
       {
         title: 'Tset annotatio',
         id: 'https://anno.ub.uni-heidelberg.de/123-rev2',
-        modified: "2017-03-11 11:00:00",
+        modified: '2017-03-11 11:00:00',
       },
       {
         title: 'Test annotation',
         id: 'https://anno.ub.uni-heidelberg.de/123-rev3',
-        modified: "2017-03-22 22:00:00",
-      }
+        modified: '2017-03-22 22:00:00',
+      },
     ],
     hasReply: [
       {
@@ -155,18 +168,18 @@ window.annotations = [
           value: 'Too strong, actually',
         },
         target: {
-          id: 'https://anno.ub.uni-heidelberg.de/123'
-        }
-      }
-    ]
+          id: 'https://anno.ub.uni-heidelberg.de/123',
+        },
+      },
+    ],
   },
   {
     body: [],
     target: [
       {
         selector: {
-            type: "SvgSelector",
-            value:
+          type: 'SvgSelector',
+          value:
 `<svg xmlns="http://www.w3.org/2000/svg">
 <rect x="0" y="0" width="200" height="200"/>
 <rect x="200" y="200" width="200" height="200"/>
@@ -175,8 +188,8 @@ window.annotations = [
 <rect x="800" y="800" width="200" height="200"/>
 <rect x="800" y="1000" width="224" height="24"/>
 </svg>`,
-        }
+        },
       },
     ],
-  }
-]
+  },
+];
