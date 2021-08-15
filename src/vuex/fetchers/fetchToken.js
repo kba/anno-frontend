db0a6b82d097b2b55eda45d9b32af15e9201fdca
mk-pmb <mk-pmb@users.noreply.github.com>
2021-08-15 21:28:31 +0200

    Fetch token: Accept HTTP 401 as just not logged in. [ubhd:178]

--- src/vuex/fetchers/fetchToken.js
+++ src/vuex/fetchers/fetchToken.js
@@ -5,8 +5,16 @@ const axios = require('axios');
 
 const browserWindow = window; // eslint-disable-line no-undef
 
+function orf(x) { return (x || false); }
 function unixTime() { return Math.floor(Date.now() / 1e3); }
 function isExpired(token) { return (unixTime() > token.exp); }
+
+
+function acceptHttp401(err) {
+  const resp = orf(orf(err).response);
+  if (resp.status === 401) { return 401; }
+  throw err;
+}
 
 
 async function fetchToken(vuexApi) {
@@ -22,11 +30,16 @@ async function fetchToken(vuexApi) {
     }
   }
 
-  const newToken = (await axios.get(state.tokenEndpoint, {
+  const tokenReply = await (axios.get(state.tokenEndpoint, {
     // maxRedirects: 0, // does not work in the browser
     withCredentials: 1, // without it, xhr won't set cookies for CORS
-  })).data;
-  if (!newToken) { throw new Error('No token received!'); }
+  }).then(null, acceptHttp401));
+  if (tokenReply === 401) { return; } // Not logged in
+  const newToken = tokenReply.data;
+  if (!newToken) {
+    console.error('Logged in but no token received:', tokenReply);
+    throw new Error('No token received!');
+  }
   await commit('SET_TOKEN', newToken);
 }
 
