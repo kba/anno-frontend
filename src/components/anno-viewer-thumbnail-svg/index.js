95f011b47b91e63c5823972f70f8848b08506e19
mk-pmb <mk-pmb@users.noreply.github.com>
2021-10-08 07:16:21 +0200

    Fix thumbnail selector region colors

--- src/components/anno-viewer-thumbnail-svg/index.js
+++ src/components/anno-viewer-thumbnail-svg/index.js
@@ -1,12 +1,42 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+'use strict';
+
+const getOwn = require('getown');
+
+const xrxStyleProps = {
+  thumbStrokeColor:   { type: [String, undefined] },
+  thumbFillColor:     { type: [String, undefined] },
+};
+
+
 module.exports = {
+  /* eslint-disable global-require */
   template: require('./thumb.html'),
+  /* eslint-enable global-require */
+
   props: {
     svgTarget:    { type: [Object] },
     sizePx:       { type: [Number], default: 36 },
     widthPx:      { type: [Number, undefined] },
     heightPx:     { type: [Number, undefined] },
 
-    thumbStrokeColor:   { type: [String, undefined] },
-    thumbFillColor:     { type: [String, undefined] },
+    ...xrxStyleProps,
+
   },
-}
+
+  computed: {
+
+    xrxStyle() {
+      const thumb = this;
+      const appCfg = thumb.$store.state;
+      const st = {};
+      Object.keys(xrxStyleProps).forEach(function add(propName) {
+        const opt = propName.replace(/^thumb(\w)/,
+          function fmt(m, l) { return (m && l.toLowerCase()); });
+        st[opt] = getOwn(thumb, propName, appCfg[propName]);
+      });
+      return st;
+    },
+
+  },
+};
