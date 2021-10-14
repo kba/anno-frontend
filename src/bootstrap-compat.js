0a3de37a531b1c09b271f50dd8b59e5e6c11ad7e
mk-pmb <mk-pmb@users.noreply.github.com>
2021-10-14 21:44:51 +0200

    Fix versions dropdown [ubhd:228]

--- src/bootstrap-compat.js
+++ src/bootstrap-compat.js
@@ -8,9 +8,19 @@ const cfg = { // Default config. will be updated in-place later.
 };
 
 const defaultTagsByBsMajorVer = {
+
   '3': {
+    dropdownWrapperCls: [
+      'dropdown',
+      // ^-- https://getbootstrap.com/docs/3.3/components/#dropdowns
+
+      // Required to allow dropdowns to integrate into btn-groups:
+      'btn-group btn-group-sm',
+    ],
     dropdownMenu: 'ul',
     dropdownMenuItem: 'li',
+    dropdownMenuItemCls: '',
+    dropdownMenuItemPad: 'a',
 
     modalHeaderTitle: 'h4',
 
@@ -18,11 +28,23 @@ const defaultTagsByBsMajorVer = {
       annoListSortMenu: { clsAll: 'btn btn-sm', clsChosen: 'btn-primary' },
     },
   },
+
   '4': {
+    dropdownWrapperCls: [
+      'btn-group btn-group-sm',
+      // ^-- https://getbootstrap.com/docs/4.0/components/button-group/#nesting
+
+      // Not required for bootstrap but for our own stylesheets:
+      'dropdown',
+    ],
+
     dropdownMenu: 'div',
     dropdownMenuItem: 'a',
     // ul+li seems to work for BS4 as well and would be more semantic,
     // but I couldn't find official compatibility info in the BS4 docs.
+
+    dropdownMenuItemCls: 'dropdown-item',
+    dropdownMenuItemPad: 'span', // should be omitted once we drop BS3
 
     modalHeaderTitle: 'h5',
 
@@ -30,6 +52,7 @@ const defaultTagsByBsMajorVer = {
       annoListSortMenu: { clsAll: 'dropdown-item', clsChosen: 'active' },
     },
   },
+
 };
 
 function thr0w(e) { throw e; }
