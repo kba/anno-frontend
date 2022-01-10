9802a66c85bab634bb873b9f268a231c79850eb4
mk-pmb <mk-pmb@users.noreply.github.com>
2022-01-10 04:48:06 +0100

    anno-editor-debug-panel: auto-decode JWT tokens

--- src/components/anno-editor-debug-panel/index.js
+++ src/components/anno-editor-debug-panel/index.js
@@ -3,6 +3,7 @@
 /* eslint-disable global-require */
 
 const loMapValues = require('lodash.mapvalues');
+const jwtDecode = require('jwt-decode');
 
 const validateEditorFields = require('../anno-editor/validateEditorFields.js');
 
@@ -10,6 +11,8 @@ const validateEditorFields = require('../anno-editor/validateEditorFields.js');
 function contextAsFirstArg(func) {
   return function proxy(...args) { return func(this, ...args); };
 }
+
+function err2str(f, x) { try { return f(x); } catch (e) { return String(e); } }
 
 
 module.exports = {
@@ -40,6 +43,16 @@ module.exports = {
         editing: u,
         localizations: u,
       };
+      Object.keys(cfg).forEach(function optimize(key) {
+        const val = cfg[key];
+        if (val === undefined) {
+          delete cfg[key];
+          return;
+        }
+        if (val && /token$/ig.test(key)) {
+          cfg[key + ' » jwtDecode'] = err2str(() => jwtDecode(val));
+        }
+      });
       return cfg;
     },
 
