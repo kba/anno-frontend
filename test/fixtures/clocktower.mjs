1eb8e9016957507ae482a79507fe872911d6b071
mk-pmb <mk-pmb@users.noreply.github.com>
2022-05-07 06:24:31 +0200

    [tests] Add clocktower fixture

---
+++ test/fixtures/clocktower.mjs
@@ -0,0 +1,20 @@
+﻿export default { "body": {
+    "format": "text/html",
+    "type": "TextualBody",
+    "value": "<p>hat Zeit.</p>"
+  },
+  "collection": "default",
+  "doi": null,
+  "id": "",
+  "replyTo": null,
+  "rights": "https://creativecommons.org/licenses/by/4.0/",
+  "target": {
+    "scope": "http://anno.test/displayAnnotations",
+    "selector": {
+      "type": "SvgSelector",
+      "value": "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n  width=\"986\">\n  <rect x=\"155.29500000000002\" y=\"88.0575588989258\" width=\"90.38333333333335\" height=\"87.09666666666666\" />\n</svg>"
+    },
+    "source": "https://upload.wikimedia.org/wikipedia/commons/d/da/Universitaet_Heidelberg_%28Karl_Lange%29_1896.jpg"
+  },
+  "title": "Uhr"
+};
