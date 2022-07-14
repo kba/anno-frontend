3f40144ae54d251fc048a28d7171b83caeb82ebe
mk-pmb <mk-pmb@users.noreply.github.com>
2022-07-14 17:41:53 +0200

    Add test fixture for mintDOI response.

---
+++ test/mock.server/mintdoi.success.excerpt.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line spaced-comment
+/*jslint indent: 2, browser: true */
+setTimeout(function install() {
+  window.annoApp.$store.state.debugStubMintDoiResponse = {
+    "minted": [
+      {
+        "minted": {
+          "@context": "http://www.w3.org/ns/anno.jsonld",
+          "type": "Annotation",
+          "id": "https://anno.ub.uni-heidelberg.de/anno/anno/PIG8QUudQzSkAgGOZ8ADsQ",
+          "title": "…",
+          "collection": "diglit",
+          "target": "…",
+          "creator": "…",
+          "created": "2022-06-25T17:27:34.236Z",
+          "modified": "2022-06-25T17:27:34.236Z",
+          "body": "…",
+          "rights": "https://creativecommons.org/licenses/by/4.0/",
+          "doi": "10.11588/anno.diglit.PIG8QUudQzSkAgGOZ8ADsQ",
+          "hasVersion": ["…"],
+        },
+      },
+    ],
+  };
+}, 100);
