598bd0015dfa43993755d2e6d465dd4a370ac60d
mk-pmb <mk-pmb@users.noreply.github.com>
2022-07-27 21:17:37 +0200

    Add label for empty link relation type

--- src/components/relationlink-editor/determinePredicateCaption.js
+++ src/components/relationlink-editor/determinePredicateCaption.js
@@ -5,8 +5,9 @@ module.exports = {
     determinePredicateCaption(predicateUrl) {
       const vueElem = this;
       const { l10n } = vueElem;
+      if (!predicateUrl) { return l10n('no_data'); }
       let c = l10n('relationlink_predicate=' + predicateUrl, predicateUrl);
-      c = c.replace(/^\w+\:\/*/, '');
+      c = c.replace(/^\w+\:\/*(?=\S)/, '');
       return c;
     },
   },
