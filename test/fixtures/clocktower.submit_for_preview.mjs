08d0a84f9689131e45c39e0a87950658ea1ed27e
mk-pmb <mk-pmb@users.noreply.github.com>
2022-10-09 20:33:39 +0200

    Test: Fixtures: Add clocktower.submit_for_preview.mjs

---
+++ test/fixtures/clocktower.submit_for_preview.mjs
@@ -0,0 +1,19 @@
+﻿export default {
+  "@context": "http://www.w3.org/ns/anno.jsonld",
+  "id": "about:preview",
+  "title": "Uhr",
+  "body": {
+    "format": "text/html",
+    "type": "TextualBody",
+    "value": "<p>hat Zeit.</p>"
+  },
+  "rights": "https://creativecommons.org/licenses/by/4.0/",
+  "target": {
+    "scope": "http://anno.test/displayAnnotations",
+    "selector": {
+      "type": "SvgSelector",
+      "value": "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\"\n  width=\"986\">\n  <rect x=\"155.29500000000002\" y=\"88.0575588989258\" width=\"90.38333333333335\" height=\"87.09666666666666\" />\n</svg>"
+    },
+    "source": "https://upload.wikimedia.org/wikipedia/commons/d/da/Universitaet_Heidelberg_%28Karl_Lange%29_1896.jpg"
+  }
+};
